08536dfa2ceefba44d8e234d14940a7de6e7b0de
Mateusz-Ramotowski-Poland <83215700+Mateusz-Ramotowski-Poland@users.noreply.github.com>
2022-05-30 11:08:11 +0200

    Done: limit of fetched articles is set by user using form input. Clean code

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@ const allArticlesTag = document.querySelectorAll(".header-nav-p-span")[1];
 const allFetchedArticlesTag =
   document.querySelectorAll(".header-nav-p-span")[0];
 const articlesList = document.querySelector(".main-ul");
+const fetchArticlesNumberInput = document.querySelector('.header-nav-form-input'); // Added !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const formTag = document.querySelector('.header-nav-form');
+
 
 let allFetchedArticlesNumber = 0;
 let howManyArticlesToFetch = 15;
@@ -10,13 +13,20 @@ let isFetching = false;
 ///////////////////////Below All function callings///////////////////////
 renderArticlesAndArticlesCounterInitFunction(); //async function
 
+formTag.addEventListener('submit', function(event){
+  // inside index.html a have a validation that submitted input data: 
+  // is not an empty string and is number converted to string
+  // is not smaller than 5 and is bigger than 50
+  event.preventDefault(); // I don't reload a page after submiting the form because of this
+  howManyArticlesToFetch = parseInt(fetchArticlesNumberInput.value);
+})
+
 window.addEventListener("scroll", async function () {
   if (checkIfNearBottomOfWEbPage() && isFetching === false) {
     await getAndRenderFetchedArticles();
     renderNumberOfAllFetchedArticles();
   }
 });
-
 ///////////////////////Below All function declarations alphabetically ordered by function name///////////////////////
 function checkIfNearBottomOfWEbPage() {
   // Window.scrollY  - number of pixels that the document is currently scrolled vertically.
