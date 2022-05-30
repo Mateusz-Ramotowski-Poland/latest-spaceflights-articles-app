b49b173a61c5c63c24b90ea42ad3dfdfc3e646dd
Mateusz-Ramotowski-Poland <83215700+Mateusz-Ramotowski-Poland@users.noreply.github.com>
2022-05-30 22:52:51 +0200

    Changes: I can remove and add articles to library inside index.html. Changing textContent on button after clicking works good

--- scripts/saveAndDeleteArticleFromLibraryLocalStorage.js
+++ scripts/saveAndDeleteArticleFromLibraryLocalStorage.js
@@ -2,68 +2,60 @@ import { localStorageLibraryArticlesKey } from "./config.js";
 ///////////////////////Below All global variables declarations///////////////////////
 const listOfAllArticlesTag = document.querySelector(".main-ul");
 
-const libraryArticlesObjectsArray = JSON.parse(localStorage.getItem(localStorageLibraryArticlesKey));
+
+const libraryArticlesObjectsArray = JSON.parse(
+  localStorage.getItem(localStorageLibraryArticlesKey)
+);
+let isButtonChangedFromAddToRemoveNewly = false;
 ///////////////////////Below All function callings///////////////////////
 listOfAllArticlesTag.addEventListener("click", function (event) {
-  //below event for 'Add to Library' buttons
+  //below event for 'Add to Library' buttons and 'Remove from Library' buttons
   if (
-    !(
-      event.target.classList.contains("main-ul-article-menu-button") &&
-      event.target.textContent === "Add to Library"
-    )
-  )
-    return;
+    event.target.classList.contains("main-ul-article-menu-button") &&
+    event.target.textContent === "Add to Library"
+  ) {
+    event.target.textContent = "Remove from Library";
 
-  event.target.textContent = "Remove from Library";
+    const chosenArticle = getChosenArticle(event);
+    const newLibraryArticle = {};
 
-  const chosenArticle = getChosenArticle(event);
-  const newLibraryArticle = {};
+    newLibraryArticle.title = chosenArticle.dataset.title;
+    newLibraryArticle.publishedDate = chosenArticle.dataset.published_date;
+    newLibraryArticle.innerHTML = chosenArticle.innerHTML;
+    newLibraryArticle.id = chosenArticle.getAttribute("id");
 
-  newLibraryArticle.title = chosenArticle.dataset.title;
-  newLibraryArticle.publishedDate = chosenArticle.dataset.published_date;
-  newLibraryArticle.innerHTML = chosenArticle.innerHTML;
-  newLibraryArticle.id = chosenArticle.getAttribute("id");
+    libraryArticlesObjectsArray.push(newLibraryArticle);
+    localStorage.setItem(
+      localStorageLibraryArticlesKey,
+      JSON.stringify(libraryArticlesObjectsArray)
+    );
+    isButtonChangedFromAddToRemoveNewly = true;
+  }
 
-  libraryArticlesObjectsArray.push(newLibraryArticle);
-  localStorage.setItem(
-    localStorageLibraryArticlesKey,
-    JSON.stringify(libraryArticlesObjectsArray)
-  );
+  if (
+    event.target.classList.contains("main-ul-article-menu-button") &&
+    event.target.textContent === "Remove from Library" &&
+    isButtonChangedFromAddToRemoveNewly === false
+  ) {
+    const chosenArticle = getChosenArticle(event);
+    const idChosenArticle = chosenArticle.getAttribute("id");
 
-  console.log(libraryArticlesObjectsArray); //DElete this line after development !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    const IndexOfArticleToBeDeleted = libraryArticlesObjectsArray.findIndex(
+      (el) => el.id === idChosenArticle
+    );
+
+    libraryArticlesObjectsArray.splice(IndexOfArticleToBeDeleted, 1);
+
+    localStorage.setItem(
+      "libraryArticles",
+      JSON.stringify(libraryArticlesObjectsArray)
+    );
+
+    event.target.textContent = "Add to Library";
+  }
+
+  isButtonChangedFromAddToRemoveNewly = false
 });
-
-// this event is also fired after clicking - I have problem with text on button and also with that That I can add and soon delete the same article
-// solution maybe use timers - after for example 10 ms change text content of button,not immidiatyly
-/* listOfAllArticlesTag.addEventListener("click", function (event) {
-  //below event for 'Remove from Library' buttons
-  if (
-    !(
-      event.target.classList.contains("main-ul-article-menu-button") &&
-      event.target.textContent === "Remove from Library"
-    )
-  )
-    return;
-
-
-
-  const chosenArticle = getChosenArticle(event);
-  const idChosenArticle = chosenArticle.getAttribute("id");
-
-  console.log(typeof idChosenArticle);
-  console.log(typeof libraryArticlesObjectsArray[0].id);
-  const IndexOfArticleToBeDeleted = libraryArticlesObjectsArray.findIndex(
-    (el) => el.id === idChosenArticle
-  );
-
-  localStorage.setItem(
-    "libraryArticles",
-    JSON.stringify(libraryArticlesObjectsArray)
-  );
-
-  event.target.textContent = "Add to Library";
-  console.log(libraryArticlesObjectsArray); //DElete this line after development !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-}); */
 ///////////////////////Below All function declarations ordered alphabetically by function name///////////////////////
 function getChosenArticle(event) {
   return event.target.closest(".main-ul-article");
